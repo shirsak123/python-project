b893339c28cc56e225f747240cff955ccd895450
shirsak123 <shisak.shrestha@gmail.com>
2019-11-18 08:53:44 +0545

    this my new python program

---
+++ 2D.py
@@ -0,0 +1,4 @@
+A=[[1,2,3],[4,5,6]]
+for row in A:
+    for element in row:
+        print(element)
---
+++ function.py
@@ -0,0 +1,19 @@
+def add ():
+    sum_ =a+b
+    return sum_
+a=2
+b=3
+print (add())
+
+def sum(a,b):
+    sum_=a+b
+    return sum_
+z=sum(3,5)
+print(z)
+
+
+
+
+
+
+
